977a28ae4d683637113396a7953ff1e1ee4715e3
Evgeny Rodionov <bitsinmyhead@gmail.com>
2021-11-30 00:15:49 +0300

    three: tweaked scene

--- three/MainScene.tsx
+++ three/MainScene.tsx
@@ -1,96 +1,29 @@
-import React, { Suspense, useRef, useState } from "react";
-import { Canvas, SpotLightProps } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
 import AtticModel from "./AtticModel";
-import { OrbitControls, softShadows, useHelper } from "@react-three/drei";
-import { BoxHelper, PointLightHelper, SpotLightHelper } from "three";
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import SceneLights from "./SceneLights";
 
 // softShadows();
 
-const SpotLightCS = (props: SpotLightProps) => {
-  const lightRef = useRef(null!);
-
-  useHelper(lightRef, SpotLightHelper, 2, "red");
-
-  return (
-    <spotLight
-      ref={lightRef}
-      castShadow
-      shadowBias={-0.01}
-      position={props.position}
-      intensity={props.intensity}
-      shadow-mapSize-width={1024}
-      shadow-mapSize-height={1024}
-      shadow-camera-far={100}
-      shadow-camera-left={-30}
-      shadow-camera-right={30}
-      shadow-camera-top={20}
-      shadow-camera-bottom={-10}
-    />
-  );
-};
-
-const Lights = () => {
-  const lightRef = useRef(null!);
-  const lightRef2 = useRef(null!);
-  const lightRef3 = useRef(null!);
-
-  useHelper(lightRef, PointLightHelper, 2, "red");
-  useHelper(lightRef2, PointLightHelper, 2, "red");
-  useHelper(lightRef3, BoxHelper, 2, "red");
-
-  return (
-    <>
-      <rectAreaLight
-        width={10}
-        height={10}
-        position={[0, 5, 30]}
-        intensity={10}
-      />
-      <pointLight
-        ref={lightRef}
-        castShadow
-        color={"#fbeebe"}
-        shadowBias={-0.05}
-        position={[13, 5, -12]}
-        intensity={0.05}
-        distance={30}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={100}
-        shadow-camera-left={-40}
-        shadow-camera-right={40}
-        shadow-camera-top={20}
-        shadow-camera-bottom={-10}
-      />
-      <pointLight
-        ref={lightRef2}
-        castShadow
-        color={"#fbeebe"}
-        shadowBias={-0.05}
-        position={[13, 5, 9]}
-        distance={30}
-        intensity={0.05}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={100}
-        shadow-camera-left={-40}
-        shadow-camera-right={40}
-        shadow-camera-top={20}
-        shadow-camera-bottom={-10}
-      />
-    </>
-  );
-};
-
 const MainScene = () => {
   return (
-    <Canvas camera={{ position: [-5, 5, 14], fov: 90 }} shadows>
-      <color attach="background" args={["#f6c962"]} />
-      <OrbitControls />
-      <fog attach="fog" args={["white", 0, 60]} />
-      <ambientLight intensity={0.02} />
-      <Lights />
+    <Canvas camera={{ position: [-5, 5, 14], fov: 80 }} shadows>
+      <OrbitControls makeDefault />ß
+      <fog attach="fog" args={["#695d55", 0, 70]} />
+      <Sky
+        turbidity={8}
+        rayleigh={6}
+        mieCoefficient={0.005}
+        mieDirectionalG={0.8}
+        distance={3000}
+        sunPosition={[1, 0.01, -0.6]}
+        inclination={0.49}
+        azimuth={0.25}
+      />
+      <SceneLights />
       <Suspense fallback={null}>
+        <Environment preset="warehouse" />
         <AtticModel />
       </Suspense>
     </Canvas>
