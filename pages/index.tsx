61b3200f076f873891c9f49629ba0021ed6f497b
Evgeny Rodionov <bitsinmyhead@gmail.com>
2022-01-01 22:53:01 +0300

    impoved layout adaptivness, added background

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Hero = () => {
           </div>
           <div className="profile__info">
             <div className="profile__bio">
-              <h3 className="profile__name">евгений Родионов</h3>
+              <h3 className="profile__name">евгений родионов</h3>
               <span className="profile__title">фронтенд разработчик</span>
             </div>
             <h1 className="profile__headline">создаю ui даже во сне</h1>
@@ -56,38 +56,31 @@ const Hero = () => {
   );
 };
 
-// <div className="project__brief">
-//   <span className="project__subtext">
-//     {convertProjectIdToSubtext(project.id)}
-//   </span>
-//   <span className="project__subtext project__subtext--size-small">
-//     {"stck: " + project.stack.join(" / ")}
-//   </span>
-// </div>
-// <div className="project__brief">
-//   <span className="project__subtext project__subtext--size-small">
-//     {"st_" + project.status}
-//   </span>
-// </div>
-
 const AnyProject = (project: Project) => {
   return (
     <div className="project">
       <div className="project__showcase">
-        <div className="project__image">
-          <Image
-            src={project.showcase_pic}
-            alt={project.title}
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
+        <Image
+          src={project.showcase_pic}
+          alt={project.title}
+          layout="responsive"
+          width={2000}
+          height={2000}
+          objectFit="contain"
+        />
       </div>
       <div className="project__details">
         <h1 className="project__title">{project.title}</h1>
         <span className="project__description">{project.description}</span>
         <div className="project__actions">
           <a className="button" href={project.source_url}>
+            <svg
+              className="button_icon"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+            </svg>
             source код
           </a>
           {project.demo_url && (
@@ -122,6 +115,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
+      {/* <div className="fade"></div> */}
       <Projects />
     </div>
   );
